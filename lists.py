eaecf6eba37510388a7cabb656480b61ba76d856
Darryl <dar.diapolet@gmail.com>
2020-01-20 21:48:45 +0800

    Lists update

--- lists.py
+++ lists.py
@@ -1,10 +1,11 @@
 
 countries = ['Philippines', 'Korea', 'Japan', 'Thailand', 'USA'];
+cities = ['Tokyo', 'Manila', 'Bangkok']
 
 def get_countries():
 	return countries
 
-print(get_countries())
+print("Countries", get_countries())
 
 
 def get_index_of(list, value):
@@ -30,7 +31,7 @@ def remove_country(list, value):
 	list.remove(value)
 	return list
 
-print("Remove", remove_list(countries, 'Philippines'))
+print("Remove", remove_country(countries, 'Philippines'))
 
 
 def sort_list(list):
@@ -44,3 +45,24 @@ def count_list(list, value):
 	return list.count(value)
 
 print("Count", count_list(countries, 'Japan'))
+
+
+def remove_last_value(list):
+	return list.pop()
+
+print("Removed last value", remove_last_value(countries))
+
+
+def reverse_list(list):
+	list.reverse()
+	return list
+
+print("Original List", get_countries())
+print("Reversed List", reverse_list(countries))
+
+
+def extend_list(list, list1):
+	list.extend(list1)
+	return list
+
+print("Extended lists", extend_list(countries, cities))
